acaa94fadf9190f99eb5add2bb614faa87ba77bf
José <joseleandro.devs@gmail.com>
2025-05-15 02:33:04 -0300

    Projeto em andamento fiz algumas alterações nos arquivos e adicionando interatividade no projeto

--- js/scrip.js
+++ js/scrip.js
@@ -1,18 +1,43 @@
 const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
 
-// Adiciona o evento de clique ao botão
-convertButton.addEventListener("click", convertValues);
 
 function convertValues() {
     // Seleciona o valor do input
     const inputCurrencyValue = document.querySelector(".input-currency").value;
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
+    const currencyValueConverted = document.querySelector(".currency-value");
 
-    // Valor do dólar
+    
+    // Valor do dólar e euro fictício
     const dolarToday = 5.2;
+    const euroToday = 6.2;
+    
+    // converte o valor de entrada para o formato brasileiro
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    }).format(inputCurrencyValue);
 
-    // Calcula o valor convertido
-    const convertedValue = inputCurrencyValue / dolarToday;
 
-    // Exibe o valor convertido no console
-    console.log(convertedValue);
+    if (currencySelect.value == 'dolar'){
+        //Se o select estiver selecionado o dólar faça:
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(inputCurrencyValue / dolarToday);
+    }
+    
+    if (currencySelect.value == 'euro'){
+        //Se o select estiver selecionado o euro faça:
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputCurrencyValue / euroToday);
+    }
+
+
+
 }
+// Adiciona o evento de clique ao botão
+convertButton.addEventListener("click", convertValues);
