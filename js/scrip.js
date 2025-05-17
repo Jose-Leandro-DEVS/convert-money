3001153fe23402535ebd8894877582c11b33a788
José <joseleandro.devs@gmail.com>
2025-05-17 03:43:46 -0300

    conversao do select - alterando texto e a imagem de acordo com select escolhido.

--- js/scrip.js
+++ js/scrip.js
@@ -8,11 +8,10 @@ function convertValues() {
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
     const currencyValueConverted = document.querySelector(".currency-value");
 
-    
     // Valor do dólar e euro fictício
     const dolarToday = 5.2;
     const euroToday = 6.2;
-    
+
     // converte o valor de entrada para o formato brasileiro
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
@@ -20,15 +19,15 @@ function convertValues() {
     }).format(inputCurrencyValue);
 
 
-    if (currencySelect.value == 'dolar'){
+    if (currencySelect.value == 'dolar') {
         //Se o select estiver selecionado o dólar faça:
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD",
         }).format(inputCurrencyValue / dolarToday);
     }
-    
-    if (currencySelect.value == 'euro'){
+
+    if (currencySelect.value == 'euro') {
         //Se o select estiver selecionado o euro faça:
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
@@ -36,8 +35,32 @@ function convertValues() {
         }).format(inputCurrencyValue / euroToday);
     }
 
+}
 
+function changeCurrency() {
+
+    const currencyName = document.getElementById("currency-name");
+    const currencyImg = document.querySelector(".currency-img");
+
+    // Se o select estiver selecionado o dólar faça: irá trocar a imagem e o nome do dólar
+    if (currencySelect.value == 'dolar') {
+        currencyName.innerHTML = 'Dólar americano';
+        currencyImg.src = "./assets/dolar.png";
+
+    }
+
+    // Se o select estiver selecionado o euro faça: irá trocar a imagem e o nome do euro
+    if (currencySelect.value == 'euro') {
+        currencyName.innerHTML = 'Euro';
+        currencyImg.src = "./assets/euro.png";
+    }
+
+    // Chama a função convertValues para atualizar os valores convertidos
+    convertValues();
 
 }
+// Adiciona o evento de mudança de seleção ao select
+currencySelect.addEventListener("change", changeCurrency);
+
 // Adiciona o evento de clique ao botão
 convertButton.addEventListener("click", convertValues);
